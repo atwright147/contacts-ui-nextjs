a0e4dc95f1a4f767864b96f0f03e927ac02b03a8
Andy Wright <atwright147@gmail.com>
2023-08-31 22:59:30 +0100

    test the fetch is working

--- src/app/(contacts)/contacts/page.spec.tsx
+++ src/app/(contacts)/contacts/page.spec.tsx
@@ -1,64 +1,71 @@
-import { afterEach, describe, expect, it, vi } from 'vitest';
+import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest';
 import { render, screen } from '@testing-library/react';
-import React, { Suspense } from 'react';
 import { Contact } from '../../../types/contact.types';
 import Page from './page';
 
-global.fetch = vi.fn().mockReturnValue(Promise.resolve({
-  json: () => Promise.resolve([
-    {
-      id: 1,
-      uuid: 'uuid1',
-      firstName: 'John',
-      lastName: 'Doe',
-      dateOfBirth: '1990-01-01',
-      jobTitle: 'Software Engineer',
-      bio: 'Lorem ipsum',
-      emails: [
-        {
-          id: 1,
-          contactId: 1,
-          email: 'email@example.com',
-          isPrimary: 1,
-          createdAt: '2022-01-01',
-          updatedAt: '2022-01-01',
-        },
-      ],
-      addresses: [
-        {
-          contactId: 1,
-          address1: 'address1',
-          address2: 'address2',
-          address3: 'address3',
-          city: 'city',
-          county: 'county',
-          postCode: 'postCode',
-          isPrimary: 1,
-        },
-      ],
-      comments: [
-        {
-          id: 1,
-          contactId: 1,
-          createdAt: '2022-01-01',
-          updatedAt: '2022-01-01',
-        }
-      ],
-      isFavourite: 1,
-      createdAt: '2022-01-01',
-      updatedAt: '2022-01-01',
-    } as Contact
-  ])
-}));
+describe('Contacts index page', () => {
+  afterEach(() => {
+    vi.resetAllMocks();
+  });
 
-describe('Index page', () => {
+  beforeEach(() => {
+    global.fetch = vi.fn().mockReturnValue(Promise.resolve({
+      json: () => Promise.resolve([
+        {
+          id: 1,
+          uuid: 'uuid1',
+          firstName: 'John',
+          lastName: 'Doe',
+          dateOfBirth: '1990-01-01',
+          jobTitle: 'Software Engineer',
+          bio: 'Lorem ipsum',
+          emails: [
+            {
+              id: 1,
+              contactId: 1,
+              email: 'email@example.com',
+              isPrimary: 1,
+              createdAt: '2022-01-01',
+              updatedAt: '2022-01-01',
+            },
+          ],
+          addresses: [
+            {
+              contactId: 1,
+              address1: 'address1',
+              address2: 'address2',
+              address3: 'address3',
+              city: 'city',
+              county: 'county',
+              postCode: 'postCode',
+              isPrimary: 1,
+            },
+          ],
+          comments: [
+            {
+              id: 1,
+              contactId: 1,
+              createdAt: '2022-01-01',
+              updatedAt: '2022-01-01',
+            }
+          ],
+          isFavourite: 1,
+          createdAt: '2022-01-01',
+          updatedAt: '2022-01-01',
+        } as Contact
+      ])
+    }));
+  });
+
   it('should render a heading', async () => {
+    // https://stackoverflow.com/a/76253176/633056
     render(await Page());
     const heading = await screen.findByRole('heading');
     expect(heading.innerHTML).toBe('Contacts');
   });
 
-  it('should call fetch once', async () => {
+  it('should fetch data', async () => {
+    render(await Page());
     expect(global.fetch).toHaveBeenCalledTimes(1);
-  })
+  });
 });
